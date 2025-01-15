4e578f49e47efdfba36e5eaf95cb08c88217988f
44David <david.soane@outlook.com>
2025-01-14 18:10:52 -0600

    Created 2 new socket files to try and send HTML through tcp socket

--- client.c
+++ client.c
@@ -19,30 +19,30 @@ int main(int argc, char *argv[]) {
 
     char buffer[256];
 
-    if (argc < 3) {
-        fprintf(stderr, "usage %s hostname port", argv[0]);
-        exit(0);
-    }
+    // if (argc < 3) {
+    //     fprintf(stderr, "usage %s hostname port", argv[0]);
+    //     exit(0);
+    // }
 
-    portno = atoi(argv[2]);
+    // portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0) {
         error("Error when opening socket");
     }
 
-    server = gethostbyname(argv[1]);
+    server = gethostbyname("localhost");
 
-    if (server == NULL) {
-        fprintf(stderr, "Error, host does not exist");
-        exit(0);
-    }
+    // if (server == NULL) {
+    //     fprintf(stderr, "Error, host does not exist");
+    //     exit(0);
+    // }
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
 
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_port = htons(80);
 
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         error("Error when connecting");
@@ -54,7 +54,7 @@ int main(int argc, char *argv[]) {
     // fgets(buffer, 255, stdin); // reads incoming messages from stdin
 
     // char header[] = "HTTP/1.1 200 \r\n\r\n <h1>Hello</h1>";
-    char header[] = "GET /index.html HTTP/1.1\r\n";
+    char header[] = "GET /index.html HTTP/1.1\r\nHOST:https://www.i-programmer.info\r\n\r\n";
 
     n = write(sockfd, header, strlen(header));
 
